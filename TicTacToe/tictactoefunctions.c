9c65575d872505e4f20194963488846afbff626d
hammdata <gohammdata@gmail.com>
2020-04-17 08:27:15 -0400

    Keyboard moving around the tic-tac-toe board

--- TicTacToe/tictactoefunctions.c
+++ TicTacToe/tictactoefunctions.c
@@ -51,6 +51,53 @@ int playGame(){
 
   int boardData[9]; /*Holds corresponding values*/
   int playerNum = 1; /*Determines which player moves*/
+  int inputChar; /*Gets character input*/
+  int x = 2, y = 1; /*The initial x,y coorinates in the grid*/
+  int didWin = 0; /*Game won or not*/
+  int canWeMove = 0; /*Preventing overwrites*/
+  int nMoves = 0; /*Counts the number of moves to find if its a cat game*/
+
+  initBoard(boardData);
+
+  mvprintw(8, 0, "It is Player 1's turn to place an (X)");
+  move(y,x);
+  refresh();
+
+  while (inputChar != 'q'){
+
+    if(nMoves == 9){
+      mvprintw(10,0,"CAT GAME");
+      inputChar = getch();
+      erase();
+      return 3;
+    }
+
+    inputChar = getch();
+
+    if (inputChar != ' ')
+      switch (inputChar) {
+        case KEY UP:
+          if(y == 3 \\ y ==5){
+              move(y=y-2,x);
+          }
+          break;
+        case KEY DOWN:
+          if(y == 1 || y == 3){
+            move(y=y+2,x);
+          }
+          break;
+        case KEY LEFT:
+          if(x == 10 || x == 6){
+            move(y, x=x-4);
+          }
+          break;
+        case KEY RIGHT:
+          if(x ==2 || x == 6){
+            move(y, x=x+4);
+          }
+          break;
+      }
+  }
 
 
 
